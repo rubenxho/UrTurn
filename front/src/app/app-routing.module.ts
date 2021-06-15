63796b4d9507d480875fb1ce51ad237b3a932758
Stefania Cucea <estefania.cucea@gmail.com>
2021-06-15 12:27:20 +0200

    footer-fullresponsive

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClienteColasComponent } from './pages/cliente-colas/cliente-colas.component';
+import { ClienteFavoritosComponent } from './pages/cliente-favoritos/cliente-favoritos.component';
+import { ClienteHomeComponent } from './pages/cliente-home/cliente-home.component';
+import { ClienteOpinionesComponent } from './pages/cliente-opiniones/cliente-opiniones.component';
+import { ClientePerfilComponent } from './pages/cliente-perfil/cliente-perfil.component';
+import { EmpresaColaComponent } from './pages/empresa-cola/empresa-cola.component';
+import { EmpresaHomeComponent } from './pages/empresa-home/empresa-home.component';
+import { EmpresaOpinionesPaginaComponent } from './pages/empresa-opiniones-pagina/empresa-opiniones-pagina.component';
+import { EmpresaPerfilComponent } from './pages/empresa-perfil/empresa-perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"clienteHome", component: ClienteHomeComponent },
+  {path:"empresaHome", component:EmpresaHomeComponent },
+  {path:"clienteColas", component:ClienteColasComponent },
+  {path:"empresaCola", component:EmpresaColaComponent },
+  {path:"empresaOpiniones", component:EmpresaOpinionesPaginaComponent },
+  {path:"clienteOpiniones", component:ClienteOpinionesComponent },
+  {path:"clienteFav", component:ClienteFavoritosComponent },
+  {path:"empresaPerfil", component:EmpresaPerfilComponent },
+  {path:"clientePerfil", component:ClientePerfilComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
