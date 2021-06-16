02160ad8e5e6c59b49fc81a10254da9a1405a7b5
yinguihua2014@gmail.com <Evatitan>
2021-06-16 15:34:21 +0200

    Dejamos a medias la funcionalidad tarjetas y solucionamos problema de node modules

--- front/src/app/pages/cliente-opiniones/cliente-opiniones.component.ts
+++ front/src/app/pages/cliente-opiniones/cliente-opiniones.component.ts
@@ -6,12 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cliente-opiniones.component.css'],
 })
 export class ClienteOpinionesComponent implements OnInit {
+  // atributo para el corazon de favorito
   public favorito: boolean;
   public frase: string;
+  // mesajes para modal
+  public mensajeModal: string[];
+  public mensajeModalEnviar: any[];
+
   constructor() {
+    // atributo para el corazon de favorito
     this.favorito = false;
     this.frase = '';
+    // mesajes para modal
+    this.mensajeModal = [
+      'Dar reseña',
+      'Escribe tu opinion',
+      'Enviar',
+      'Cancelar',
+    ];
+    this.mensajeModalEnviar = ['Has enviado un opinion'];
   }
+  // method para guardar a favorito
   fav() {
     console.log(this.favorito);
     this.favorito = !this.favorito;
@@ -21,5 +36,6 @@ export class ClienteOpinionesComponent implements OnInit {
       this.frase = 'Has guardado este lugar a favorito!';
     }
   }
+
   ngOnInit(): void {}
 }
--- front/src/app/pages/modal/modal.component.ts
+++ front/src/app/pages/modal/modal.component.ts
@@ -3,35 +3,33 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
+  @Input() mensajeModal: string[];
+  @Output() eventoCola = new EventEmitter<boolean>();
 
-  @Input() mensajeModal: string [];
-  @Output() eventoCola= new EventEmitter<boolean>();
-
-  constructor() { 
-  // Array mensajeModal(pos0: etiqueta modal, pos2: op1 pregunta, pos3:op2 pregunta, siguientes posiciones eventos)
-    this.mensajeModal=[]
+  constructor() {
+    // Array mensajeModal(pos0: etiqueta modal, pos2: op1 pregunta, pos3:op2 pregunta, siguientes posiciones eventos)
+    this.mensajeModal = [];
   }
 
   //Funcion para modificar cambios de estados en el home-empresa (Iniciar/detener)
-  modificar(){
-    if(this.mensajeModal[5]){
-      if(this.mensajeModal[4]){
-        this.llamarEventoCola(true)
-      }
-      else{
-        this.llamarEventoCola(false)
+  modificar() {
+    console.log('flaggg');
+
+    if (this.mensajeModal[5]) {
+      if (this.mensajeModal[4]) {
+        this.llamarEventoCola(true);
+      } else {
+        this.llamarEventoCola(false);
       }
     }
   }
 
-  llamarEventoCola(cola:boolean){
-    this.eventoCola.emit(cola)
+  llamarEventoCola(cola: boolean) {
+    this.eventoCola.emit(cola);
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
