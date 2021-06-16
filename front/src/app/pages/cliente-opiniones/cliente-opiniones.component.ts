3b5ce79232cedcf093e2bad6f01e9a9de1f1b3f3
yinguihua2014@gmail.com <Evatitan>
2021-06-16 09:50:37 +0200

    crear cliente-trajeta y cliente resena

--- front/src/app/pages/cliente-opiniones/cliente-opiniones.component.ts
+++ front/src/app/pages/cliente-opiniones/cliente-opiniones.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cliente-opiniones',
   templateUrl: './cliente-opiniones.component.html',
-  styleUrls: ['./cliente-opiniones.component.css']
+  styleUrls: ['./cliente-opiniones.component.css'],
 })
 export class ClienteOpinionesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public favorito: boolean;
+  public frase: string;
+  constructor() {
+    this.favorito = false;
+    this.frase = '';
   }
-
+  fav() {
+    console.log(this.favorito);
+    this.favorito = !this.favorito;
+    if (this.frase === 'Has guardado este lugar a favorito!') {
+      this.frase = 'Has cancelado el favorito';
+    } else {
+      this.frase = 'Has guardado este lugar a favorito!';
+    }
+  }
+  ngOnInit(): void {}
 }
--- front/src/app/pages/cliente-tarjetas/cliente-tarjetas.component.ts
+++ front/src/app/pages/cliente-tarjetas/cliente-tarjetas.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cliente-tarjetas',
   templateUrl: './cliente-tarjetas.component.html',
-  styleUrls: ['./cliente-tarjetas.component.css']
+  styleUrls: ['./cliente-tarjetas.component.css'],
 })
 export class ClienteTarjetasComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public favorito: boolean;
+  public frase: string;
+  constructor() {
+    this.favorito = false;
+    this.frase = '';
+  }
+  fav() {
+    console.log(this.favorito);
+    this.favorito = !this.favorito;
+    if (this.frase === 'Has guardado este lugar a favorito!') {
+      this.frase = 'Has cancelado el favorito';
+    } else {
+      this.frase = 'Has guardado este lugar a favorito!';
+    }
   }
 
+  ngOnInit(): void {}
 }
