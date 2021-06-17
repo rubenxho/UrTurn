ee509b35e3625605723ef75c468d210475d0764a
rubenxho <rrivasb10@gmail.com>
2021-06-17 10:41:05 +0200

    Empresa-cola

--- front/src/app/pages/empresa-cola/empresa-cola.component.ts
+++ front/src/app/pages/empresa-cola/empresa-cola.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpresaColaComponent implements OnInit {
 
+  public mensajeModalCola: string[]
+  public mensajeModalAvanzar: string[]
 
   constructor() {
-    
+    this.mensajeModalCola=["modalModificarCola","Confirmar nuevo tiempo de espera","Confirmar","Cancelar","Tiempo modificado correctamente!"]
+    this.mensajeModalAvanzar=["modalAvanzar","¿Desea avanzar la cola?","Si","No","Cola avanzada correctamente"]
   }
 
   ngOnInit(): void {
