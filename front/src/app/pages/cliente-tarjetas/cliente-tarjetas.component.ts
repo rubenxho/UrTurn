0f831f9584ca4dc9d21a71700f2f6ea09a299d2d
Evatitan <yinguihua2014@gmail.com>
2021-06-19 21:29:31 +0200

    modificar background color del nombre

--- front/src/app/pages/cliente-tarjetas/cliente-tarjetas.component.ts
+++ front/src/app/pages/cliente-tarjetas/cliente-tarjetas.component.ts
@@ -25,6 +25,7 @@ export class ClienteTarjetasComponent implements OnInit {
     // Info de trajeta: 1.foto,2.nombre,3.tiempoAprox,4.boton
     this.foto = [
       'https://www.elviajerofisgon.com/wp-content/uploads/2016/03/RestaurantesAntiguosEspa%C3%B1a_destacada-1280x720.jpg',
+      'https://img.freepik.com/vector-gratis/fondo-blanco-minimo_1393-309.jpg?size=626&ext=jpg',
       'https://estaticos-cdn.prensaiberica.es/clip/e7aca637-102b-4202-ad24-5ce54f34e1c2_16-9-aspect-ratio_default_0.jpg',
       'https://i0.wp.com/www.rutasporespana.es/blog/wp-content/uploads/2017/06/Hammam6.jpg?resize=675%2C450&ssl=1',
     ];
