88e45b28e905b00d077e94acd834e7b87e7f7522
rubenxho <rrivasb10@gmail.com>
2021-06-20 23:12:35 +0200

    routing login con header footer y arreglo nav footer

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -7,10 +7,23 @@ declare var $: any;
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public prueba:String
-  
+  public header:String
+  public estado:String
+
   constructor(){
-    this.prueba="Empresa"
+    this.header=""
+    this.estado="conectado"
+  }
+
+  tipoUsuario(usuario:string){
+    console.log("Mensaje desde app")
+    this.estado=""
+    if(usuario=="empresa"){
+      this.header="Empresa"
+    }
+    else{
+      this.header="Cliente"
+    }
   }
 
   ngOnInit() {
