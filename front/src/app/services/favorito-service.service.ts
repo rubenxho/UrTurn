a849990f35bc3417917c0bc300ebab331da40cd4
Stefania Cucea <estefania.cucea@gmail.com>
2021-06-24 12:04:08 +0200

    subo servicio fav

--- front/src/app/services/favorito-service.service.ts
+++ front/src/app/services/favorito-service.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Favoritos } from '../models/favoritos';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +12,25 @@ private url:string
     this.url="http://localhost:3000/favoritos"
   }
 
-  obtenerFav(categoria:string, cp:string, id_usuario_cliente:number):any{
-    return this.http.get(this.url + categoria + cp + id_usuario_cliente)
+  obtenerFavId( id_usuario_cliente:number):any{
+    return this.http.get(this.url + `?id=` + id_usuario_cliente)
   }
 
-  // anyadirFav(id_usuario_cliente:number, id_usuario_empresa:number, NOW()){
-    
-  //   return this.http.post(this.url , )
-  // }
+  obtenerFavCategoria(categoria:string, id_usuario_cliente:number):any{
+    return this.http.get(this.url + `?categoria=` + categoria + `&` + `?id=` + id_usuario_cliente)
+  }
+
+  obtenerFavCp(cp:string, id_usuario_cliente:number):any{
+    return this.http.get(this.url + `?cp=` + cp + `&` + `?id=` + id_usuario_cliente)
+  }
+
+  obtenerFav(categoria:string, cp:string, id_usuario_cliente:number):any{
+    return this.http.get(this.url +`?categoria=`+ categoria + `&` + `?cp=` + cp + `&` + `?id=` + id_usuario_cliente)
+  }
+
+  anyadirFav(id_usuario_cliente:number, id_usuario_empresa:number){
+    let nuevoFav:Favoritos= new Favoritos (id_usuario_cliente, id_usuario_empresa)
+    return this.http.post(this.url ,nuevoFav )
+  }
 }
+
