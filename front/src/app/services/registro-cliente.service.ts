376889521ee5536292f3dd184c5db421c80da71d
JaviDiesel <fjramos13@hotmail.com>
2021-06-24 12:18:34 +0200

    funcionalidad registro cliente

---
+++ front/src/app/services/registro-cliente.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioCliente } from '../models/usuario-cliente';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistroClienteService {
+
+  private url:string;
+
+  constructor(private http:HttpClient) { 
+    this.url = 'http://localhost:3000/cliente-registro';
+  }
+
+  public postNuevoUsuario(cliente:UsuarioCliente):any  {
+    alert(cliente.nombre_cliente);
+    return this.http.post(this.url, cliente);
+  }
+}
