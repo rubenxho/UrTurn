5113693e3f408e7fcb8d4d1f209402dd57be5629
rubenxho <rrivasb10@gmail.com>
2021-06-29 15:45:58 +0200

    despligue app

--- front/server.js
+++ front/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/urturn'))
+app.use(express.static(__dirname + '/dist/front'))
 app.get('/*',function(req,res){
-    res.sendFile("index.html", {root: __dirname +'/dist/urturn/'})
+    res.sendFile("index.html", {root: __dirname +'/dist/front/'})
 })
 
 app.listen(process.env.PORT || 8080)
