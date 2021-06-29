b2842363b7ca7fc0694ada424161a28659897fce
rubenxho <rrivasb10@gmail.com>
2021-06-29 16:02:44 +0200

    Subida Para el Despliegue Perfecta

--- front/server.js
+++ front/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/front'))
+app.use(express.static(__dirname + '/dist/urturn'))
 app.get('/*',function(req,res){
-    res.sendFile("index.html", {root: __dirname +'/dist/front/'})
+    res.sendFile("index.html", {root: __dirname +'/dist/urtun/'})
 })
 
 app.listen(process.env.PORT || 8080)
